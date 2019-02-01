18af58734e9e5f385b3863c46d0a5039a8fe77da
BenJilks <benjyjilks@gmail.com>
2019-02-01 11:24:08 +0000

    More login

--- Client/Scripts/Login.js
+++ Client/Scripts/Login.js
@@ -1,16 +1,25 @@
 
+function processLogin(data)
+{
+    if (data['status'] != 0)
+    {
+        console.log("Error: " + data['status']);
+        return;
+    }
+
+    $.cookie("uid", data['uid']);
+    window.location.href = "/dashboard";
+}
+
 function login()
 {
     let username = $("#l_username").val();
     let pwd = $("#l_pwd").val();
-    $.post("/login", {mode: "login", username: username, pwd: pwd})
+    $.post("/login", {username: username, pwd: pwd})
         .done(function(rawdata)
     {
         let data = JSON.parse(rawdata);
-        if (data['status'] != 0)
-            console.log("Error: " + data['status']);
-        else
-            window.location.href = "/dashboard";
+        processLogin(data);
     });
 }
 
@@ -19,13 +28,10 @@ function register()
     let username = $("#r_username").val();
     let pwd = $("#r_pwd").val();
     let pwd_conf = $("#r_pwd_conf").val();
-    $.post("/login", {mode: "register", username: username, pwd: pwd, pwd_conf: pwd_conf})
+    $.post("/register", {username: username, pwd: pwd, pwd_conf: pwd_conf})
         .done(function(rawdata)
     {
         let data = JSON.parse(rawdata);
-        if (data['status'] != 0)
-            console.log("Error: " + data['status']);
-        else
-            window.location.href = "/dashboard";
+        processLogin(data);
     });
 }
